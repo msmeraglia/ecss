d917e66aea2f7f22d1566cc038035cb10d1d147d
Matthew Smeraglia <matthew.smeraglia@gmail.com>
2021-08-21 08:53:49 -0500

    Simplified/reworked so that entity ids are not directly stored in data structs,
    Reworked so that entity ids are not directly stored in data structs, saving space and better cache utilization, added iterators so client code can choose to have entity returned with the data if ids are needed

--- src/collection.rs
+++ src/collection.rs
@@ -1,27 +1,29 @@
 use crate::{Component, Entity};
-
 use std::collections::HashMap;
 
 //TODO: Eventually allow client to pass custom allocator for data Vec
-pub struct Collection<T: Component> {
+pub struct Collection<T: 'static + Component + Sized> {
     entity_lookup: HashMap<Entity, usize>,
     data: Vec<T>,
+    entities: Vec<Entity>,
 }
 
-impl<T: Component> Default for Collection<T> {
+impl<T: 'static + Component + Sized> Default for Collection<T> {
     fn default() -> Self {
         Self {
-            entity_lookup: HashMap::new(),
             data: Vec::new(),
+            entities: Vec::new(),
+            entity_lookup: HashMap::new(),
         }
     }
 }
 
-impl<T: Component> Collection<T> {
+impl<T: 'static + Component + Sized> Collection<T> {
     pub fn new(size: usize) -> Self {
         Self {
-            entity_lookup: HashMap::with_capacity(size),
             data: Vec::with_capacity(size),
+            entities: Vec::with_capacity(size),
+            entity_lookup: HashMap::with_capacity(size),
         }
     }
 
@@ -29,14 +31,25 @@ impl<T: Component> Collection<T> {
         self.entity_lookup.contains_key(&entity)
     }
 
-    //TODO: What happens when its full? Return a Result<>, Option<>? Panic?
-    //This should definitely indicate to the use its full, perhaps add a flag for if Vecs can resize..
-    pub fn create(&mut self, data: T) {
-        let entity = data.entity_id();
+    pub fn create(&mut self, entity: Entity, data: T) {
         if !self.entity_lookup.contains_key(&entity) && self.data.len() < self.data.capacity() {
             self.entity_lookup.insert(entity, self.data.len());
             self.data.push(data);
+            self.entities.push(entity);
         }
+    }
+
+    pub fn entities_where<F>(&self, f: F) -> Vec<Entity>
+    where
+        F: Fn(&T) -> bool + 'static,
+    {
+        let mut entities = Vec::new();
+        for (i, item) in self.data.iter().enumerate() {
+            if f(item) {
+                entities.push(self.entities[i]);
+            }
+        }
+        entities
     }
 
     pub fn get(&self, entity: Entity) -> Option<&T> {
@@ -54,7 +67,7 @@ impl<T: Component> Collection<T> {
     }
 
     pub fn get_entities(&self) -> Vec<Entity> {
-        self.entity_lookup.keys().copied().collect()
+        self.entities.clone()
     }
 
     pub fn is_empty(&self) -> bool {
@@ -73,22 +86,35 @@ impl<T: Component> Collection<T> {
         self.data.iter_mut()
     }
 
+    pub fn iter_with_entities(&self) -> impl Iterator<Item = (Entity, &T)> {
+        self.entities
+            .iter()
+            .zip(self.data.iter())
+            .map(|(entity, item)| (*entity, item))
+            .into_iter()
+    }
+
+    pub fn iter_with_entities_mut(&mut self) -> impl Iterator<Item = (Entity, &mut T)> {
+        self.entities
+            .iter()
+            .zip(self.data.iter_mut())
+            .map(|(entity, item)| (*entity, item))
+            .into_iter()
+    }
+
     pub fn remove(&mut self, entity: Entity) {
         if self.entity_lookup.contains_key(&entity) && !self.data.is_empty() {
             let swap_id = *self.entity_lookup.get(&entity).unwrap();
             let last_node_id = self.data.len() - 1;
-
             if last_node_id > 0 {
-                // fix up the last node's hashmap value
-                let entity_to_fix = self.data[last_node_id].entity_id();
+                let entity_to_fix = self.entities[last_node_id];
                 self.entity_lookup.insert(entity_to_fix, swap_id);
-                // swap node to remove with last fixed up node then
-                // remove the last item so that no items need to be shifted
-                // aka one swap instead of potentially many
                 self.data.swap(swap_id, last_node_id);
+                self.entities.swap(swap_id, last_node_id);
             }
             self.entity_lookup.remove(&entity);
             self.data.remove(last_node_id);
+            self.entities.remove(last_node_id);
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 pub mod ecss;
-pub use crate::ecss::{ComponentType, Entity, Type, ECSS};
+pub use crate::ecss::{Component, Entity, ComponentIdType, ECSS};
 
 mod collection;
 use collection::Collection;
-
-pub use ecss_component_derive::Component;
-pub trait Component {
-    fn entity_id(&self) -> Entity;
-}
